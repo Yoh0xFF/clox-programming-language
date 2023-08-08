0a380ac2d685496d8192d67b10f02813505c59da
Ioram Gordadze <ioramgordadze@fb.com>
2023-08-08 23:17:10 +0400

    Added logical operators support.

--- compiler.c
+++ compiler.c
@@ -129,6 +129,10 @@ static void declareVariable();
 
 static void defineVariable(uint8_t global);
 
+static void and(bool canAssign);
+
+static void or(bool canAssign);
+
 static void expression();
 
 static void block();
@@ -170,7 +174,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER]    = {variable, NULL, PREC_NONE},
     [TOKEN_STRING]        = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER]        = {number, NULL, PREC_NONE},
-    [TOKEN_AND]           = {NULL, NULL, PREC_NONE},
+    [TOKEN_AND]           = {NULL, and, PREC_AND},
     [TOKEN_CLASS]         = {NULL, NULL, PREC_NONE},
     [TOKEN_ELSE]          = {NULL, NULL, PREC_NONE},
     [TOKEN_FALSE]         = {literal, NULL, PREC_NONE},
@@ -178,7 +182,7 @@ ParseRule rules[] = {
     [TOKEN_FUN]           = {NULL, NULL, PREC_NONE},
     [TOKEN_IF]            = {NULL, NULL, PREC_NONE},
     [TOKEN_NIL]           = {literal, NULL, PREC_NONE},
-    [TOKEN_OR]            = {NULL, NULL, PREC_NONE},
+    [TOKEN_OR]            = {NULL, or, PREC_OR},
     [TOKEN_PRINT]         = {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN]        = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER]         = {NULL, NULL, PREC_NONE},
@@ -574,6 +578,25 @@ static void defineVariable(uint8_t global) {
   }
 
   emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+static void and(bool canAssign) {
+  int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+  emitByte(OP_POP);
+  parsePrecedence(PREC_AND);
+  patchJump(endJump);
+}
+
+static void or(bool canAssign) {
+  int elseJump = emitJump(OP_JUMP_IF_FALSE);
+  int endJump = emitJump(OP_JUMP);
+
+  patchJump(elseJump);
+  emitByte(OP_POP);
+
+  parsePrecedence(PREC_OR);
+  patchJump(endJump);
 }
 
 static void expression() {
