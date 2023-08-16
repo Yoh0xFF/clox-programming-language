4c22e6264a930b76904482810ecba659df97d040
Ioram Gordadze <ioramgordadze@fb.com>
2023-08-17 02:11:03 +0400

    Add function object.

--- object.c
+++ object.c
@@ -15,6 +15,16 @@ static ObjString *allocateString(char *chars, int length, uint32_t hash);
 
 static uint32_t hashString(const char *key, int length);
 
+static void printFunction(ObjFunction *function);
+
+ObjFunction  *newFunction() {
+  ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+  function->arity = 0;
+  function->name = NULL;
+  initChunk(&function->chunk);
+  return function;
+}
+
 ObjString *takeString(char *chars, int length) {
   uint32_t hash = hashString(chars, length);
 
@@ -43,6 +53,9 @@ ObjString *copyString(const char *chars, int length) {
 
 void printObject(Value value) {
   switch (OBJ_TYPE(value)) {
+    case OBJ_FUNCTION:
+      printFunction(AS_FUNCTION(value));
+      break;
     case OBJ_STRING:
       printf("%s", AS_CSTRING(value));
       break;
@@ -77,4 +90,8 @@ static uint32_t hashString(const char *key, int length) {
     hash *= 16777619;
   }
   return hash;
+}
+
+static void printFunction(ObjFunction *function) {
+  printf("<fn %s>", function->name->chars);
 }
--- object.h
+++ object.h
@@ -2,9 +2,11 @@
 #define CLOX_OBJECT_H
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 typedef enum {
+  OBJ_FUNCTION,
   OBJ_STRING,
 } ObjType;
 
@@ -12,6 +14,13 @@ struct Obj {
   ObjType type;
   struct Obj *next;
 };
+
+typedef struct {
+  Obj obj;
+  int arity;
+  Chunk chunk;
+  ObjString *name;
+} ObjFunction;
 
 struct ObjString {
   Obj obj;
@@ -26,10 +35,14 @@ static inline bool isObjType(Value value, ObjType type) {
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value) ((ObjFunction *) AS_OBJ(value))
 #define AS_STRING(value) ((ObjString *) AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString *) AS_OBJ(value))->chars)
+
+ObjFunction  *newFunction();
 
 ObjString *takeString(char *chars, int length);
 
